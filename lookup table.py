cf4916a27d67ea28dad773c3118a23103e51da58
khadharbasha <bashaasjack@gmail.com>
2020-02-14 09:07:02 +0530

    Update and rename split cols.py to lookup table.py

--- lookup table.py
+++ lookup table.py
@@ -45,3 +45,38 @@ data = split_data.to_list()
 names = ['Modified Main Service','Modified Service Name']
 new_df = pd.DataFrame(data, columns=names)
 result = pd.concat([df, new_df], axis=1, sort=False)
+
+
+## Below procedure create a dict key value pair lookup table
+df=pd.read_excel('POC Service Code Mappings.xlsx')
+
+                    # Creating Key
+df['Main Service Names'] = df['Main Service Names'].str.strip()
+df['Service Names'] = df['Service Names'].str.strip()
+df['Key'] = df[['Main Service Names', 'Service Names']].agg('-'.join, axis=1)
+
+                    # Creating Value
+df['Standard Main Service Name'] = df['Standard Main Service Name'].str.strip()
+df['Standard Service Name'] = df['Standard Service Name'].str.strip()
+df['Value'] = df[['Standard Main Service Name', 'Standard Service Name', 'Use / Ignore']].agg('-'.join, axis=1)
+
+                    # Creating dict
+mydict = dict(zip(df.Key, df.Value))
+mydict
+
+# Using dict in Input
+input_data = pd.read_excel('Input.xlsx')
+input_data['key'] = input_data['Main Service Name'].str.strip() + '-' + input_data['Service Name'].str.strip()
+
+def mapping(df2):
+    #for key,value in mydict.items():
+       # if key in df2['Main Service Name'] & key in df2['Service Name']:
+    #key = df2['Main Service Name'].str.strip() + '-' + df2['Service Name']
+    return mydict.get(df2, 'Invalid')
+
+input_data['Value'] = input_data.key.apply(mapping)
+
+input_data[['Main Service','Sub Service','Use/Ignore']]=input_data.Value.str.split('-',expand=True)
+
+
+model_data = input_data[['Main Service Name', 'Sub Service']]
